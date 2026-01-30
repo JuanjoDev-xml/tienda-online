894100fbb5a77877a84f3c239f924e48e588e1f8
Aarón <netaru3@gmail.com>
2026-01-30 19:39:56 -0300

    Change PaymentId to paymentId in schema

--- base_de_datos_mongo/mongo_pagos.js
+++ base_de_datos_mongo/mongo_pagos.js
@@ -6,7 +6,7 @@ try{await mongoose.connect(process.env.URL)}
 catch(error){console.log("error:",error)}
 
 let esquema=new mongoose.Schema({
- PaymentId: String,
+ paymentId: String,
 });
 
 export let log_pagos_id= mongoose.model("pagos-id-tienda-online",esquema)
