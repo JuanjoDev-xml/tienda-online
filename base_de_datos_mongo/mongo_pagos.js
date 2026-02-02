c9f449d60f6a52dd20011b3692eb79ee03ccfdaa
Aarón <netaru3@gmail.com>
2026-02-02 14:25:42 -0300

    Update paymentId field type and enforce uniqueness
    Changed paymentId type from Number to String and added unique index.

--- base_de_datos_mongo/mongo_pagos.js
+++ base_de_datos_mongo/mongo_pagos.js
@@ -6,7 +6,11 @@ try{await mongoose.connect(process.env.URL)}
 catch(error){console.log("error:",error)}
 
 let esquema=new mongoose.Schema({
- paymentId:{type: Number, unique: true},
+ paymentId:{type: String}
 });
+
+esquema.index({paymentId:1},{unique:true})
+
+await mongoose.connection.syncIndexes();
 
 export let log_pagos_id= mongoose.model("pagos-id-tienda-online",esquema)
