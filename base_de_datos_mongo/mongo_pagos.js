e4a48928a9dc13d9eeedd7aa0987dca284a8d07d
Aarón <netaru3@gmail.com>
2026-02-02 15:40:49 -0300

    Refactor mongo_pagos.js for consistency and clarity

--- base_de_datos_mongo/mongo_pagos.js
+++ base_de_datos_mongo/mongo_pagos.js
@@ -1,16 +1,18 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config()
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-try{await mongoose.connect(process.env.URL)}
-catch(error){console.log("error:",error)}
+await mongoose.connect(process.env.URL);
 
-let esquema=new mongoose.Schema({
- paymentId:{type: String,required:true,unique:true}
+const esquema = new mongoose.Schema({
+  paymentId: { type: String, required: true }
 });
 
-esquema.index({paymentId:1},{unique:true})
+esquema.index({ paymentId: 1 }, { unique: true });
 
-await mongoose.connection.syncIndexes();
+export const log_pagos_id = mongoose.model(
+  "pagos-id-tienda-online",
+  esquema
+);
 
-export let log_pagos_id= mongoose.model("pagos-id-tienda-online",esquema)
+await log_pagos_id.init(); 
