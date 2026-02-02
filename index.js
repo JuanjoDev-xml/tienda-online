c37b418ecd0c7b570c921189ece26f2cfe05bee4
Aarón <netaru3@gmail.com>
2026-02-02 16:45:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -465,10 +465,6 @@ app.post("/webhook", async function(req, res) {
 
                    console.log("paymentId:",paymentId)
 
-                 let payment_base_datos= await log_pagos_id.find({paymentId:paymentId})
-                   console.log("payment base de datos:",payment_base_datos);
-                 if(payment_base_datos.length!==0){return}
-
             await log_pagos_id.create({paymentId: paymentId})
 
            
