4961b68cd30cbab2045d9bbe49467bec14a11507
Aarón <netaru3@gmail.com>
2026-02-13 14:10:43 -0300

    Update mongo-reembolsos.js

--- base_de_datos_mongo/mongo-reembolsos.js
+++ base_de_datos_mongo/mongo-reembolsos.js
@@ -5,6 +5,8 @@ dotenv.config()
 try{await mongoose.connect(process.env.URL)}
 catch(error){console.log("error:",error)}
 
+
+
 let esquema=new mongoose.Schema({
     usuario: String,
     productos:{type: Array, required: false},
@@ -15,7 +17,14 @@ let esquema=new mongoose.Schema({
  producto_id: Number,
  local_ubicacion: String,
  reembolsado: Boolean,
+ paymentId: String,
  Date: Date
 });
 
+esquema.index({ paymentId: 1 }, { unique: true });
+
 export let log_reembolsos= mongoose.model("reembolsos-tienda-online",esquema)
+
+
+
+await log_reembolsos.init(); 
